3379f02abd98b65b8e99d5cd2c361ce47c896a06
YT <362591904@qq.com>
2019-03-28 23:25:20 +0800

    学完了第四章

---
+++ chapter04/Duple.py
@@ -0,0 +1,15 @@
+dimensions = (100, 80)
+print(dimensions[0])
+print(dimensions[1])
+
+print("----------------------------------------")
+for val in dimensions:
+    print(val)
+
+print("----------------------------------------")
+dimensions = (100, 80)
+dimensions = (200, 100)
+for val in dimensions:
+    print(val)
+print("----------------------------------------")
+
