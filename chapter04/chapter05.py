c20ea27aca748578e5605e34ec10741897ff035b
YT <362591904@qq.com>
2019-03-30 23:03:15 +0800

    学

---
+++ chapter04/chapter05.py
@@ -0,0 +1,16 @@
+print("--------------------------------------------------------")
+cars = ['bmw', 'benz', 'audi']
+if "bmw" in cars:
+    print("在里面")
+if "bmwa" not in cars:
+    print("不在里面")
+else:
+    print("在里面")
+
+print("--------------------------------------------------------")
+cars = []
+if (cars):
+    print("car")
+else:
+    print("空的")
+
